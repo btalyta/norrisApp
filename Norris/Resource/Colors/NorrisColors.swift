490f47b3a58a203e701158e8f83f69de470b02c2
Barbara Souza <b.barbaratallyta@gmail.com>
2020-04-12 12:51:53 -0300

    Add colors

---
+++ Norris/Resource/Colors/NorrisColors.swift
@@ -0,0 +1,13 @@
+//
+//  NorrisColors.swift
+//  Norris
+//
+//  Created by Bárbara Souza on 12/04/20.
+//  Copyright © 2020 Bárbara Souza. All rights reserved.
+//
+
+import UIKit
+
+class NorrisColors {
+    static let tagColor = UIColor(displayP3Red: 240/255, green: 113/255, blue: 97/255, alpha: 1.0)
+}
