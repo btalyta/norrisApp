0621f14f0f11462f46f44d3e64df784550d3a812
Barbara Souza <b.barbaratallyta@gmail.com>
2020-04-11 23:31:51 -0300

    Add APIError

--- Norris/ListView/ListViewController.swift
+++ Norris/ListView/ListViewController.swift
@@ -15,6 +15,4 @@ class ListViewController: UIViewController {
         view.backgroundColor = .blue
     }
 
-
 }
-
---
+++ Norris/Networking/APIError.swift
@@ -0,0 +1,35 @@
+//
+//  APIError.swift
+//  Norris
+//
+//  Created by Bárbara Souza on 11/04/20.
+//  Copyright © 2020 Bárbara Souza. All rights reserved.
+//
+
+import Foundation
+
+enum APIError: Error, Equatable {
+    case jsonConversionFailure
+    case invalidData
+    case timeout
+    case unauthorized
+    case server
+    case request
+    
+    var localizedDescription: String {
+        switch self {
+        case .invalidData:
+            return "Invalid data."
+        case .timeout:
+            return "Connection error."
+        case .unauthorized:
+            return "Unauthorized."
+        case .jsonConversionFailure:
+            return "JSON conversion failure."
+        case .server:
+            return "Unavailable service."
+        case .request:
+            return "Invalid request."
+        }
+    }
+}
