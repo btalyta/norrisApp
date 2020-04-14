0192a0d90f2dbb4a0a7dea64bb781018e197ecda
Barbara Souza <b.barbaratallyta@gmail.com>
2020-04-13 22:11:07 -0300

    Add tests to images

---
+++ NorrisTests/Resource/Images/NorrisImagesTests.swift
@@ -0,0 +1,23 @@
+//
+//  NorrisImagesTests.swift
+//  NorrisTests
+//
+//  Created by Bárbara Souza on 13/04/20.
+//  Copyright © 2020 Bárbara Souza. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Norris
+
+class NorrisImagesTests: QuickSpec {
+    override func spec() {
+        describe("#shareIcon") {
+            it("return correct image") {
+                let expectedImage = UIImage(named: "shareIcon")
+                expect(NorrisImages.shareIcon).to(equal(expectedImage))
+            }
+        }
+    }
+}
