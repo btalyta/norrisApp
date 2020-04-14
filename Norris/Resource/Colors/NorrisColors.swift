1cbaf78b1626bd5012e2f5f0a2acbb56e2f5f078
Barbara Souza <b.barbaratallyta@gmail.com>
2020-04-13 22:11:21 -0300

    Add colors

--- Norris/Resource/Colors/NorrisColors.swift
+++ Norris/Resource/Colors/NorrisColors.swift
@@ -9,5 +9,8 @@
 import UIKit
 
 class NorrisColors {
-    static let tagColor = UIColor(displayP3Red: 240/255, green: 113/255, blue: 97/255, alpha: 1.0)
+    static let tagColor = UIColor(displayP3Red: 63/255, green: 167/255, blue: 214/255, alpha: 1.0)
+    static let errorColor = UIColor(displayP3Red: 240/255, green: 113/255, blue: 97/255, alpha: 1.0)
+    static let tagTextColor = UIColor.white
+    static let factTextColor = UIColor.black
 }
