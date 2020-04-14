aace253930908a337ffdc1460ed98a24f07f4180
Barbara Souza <b.barbaratallyta@gmail.com>
2020-04-13 21:40:27 -0300

    Add protocols

---
+++ Norris/FactsView/FactsProtocols.swift
@@ -0,0 +1,29 @@
+//
+//  FactsProtocols.swift
+//  Norris
+//
+//  Created by Bárbara Souza on 13/04/20.
+//  Copyright © 2020 Bárbara Souza. All rights reserved.
+//
+
+import Foundation
+
+protocol FactsRepositoryProtocols {
+    func requestCategories(completion: @escaping ((Result<CategoriesModel, APIError>) -> Void))
+    func requestFact(with category: String, completion: @escaping ((Result<FactModel, APIError>) -> Void))
+    func requestCollection(with text: String, completion: @escaping ((Result<FactsCollectionModel, APIError>) -> Void))
+}
+
+protocol FactsPresenterProtocol: class {
+    var viewController: FactsViewControllerProtocol? { get set}
+    func viewDidLoad()
+    func didSelectCard(with index: IndexPath)
+    func wantsToSearch(category: String)
+    func wantsToSearch(text: String)
+}
+
+protocol FactsViewControllerProtocol: class {
+    func showFacts(viewModel: FactsViewModel)
+    func showError()
+    func wantsToShare(url: String)
+}
