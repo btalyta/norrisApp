3a4ac7c6223642373c17e26d8bfc7addf81ee12f
Barbara Souza <b.barbaratallyta@gmail.com>
2020-04-13 22:10:17 -0300

    Add extension

---
+++ Norris/Helper/String+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  String+Ext.swift
+//  Norris
+//
+//  Created by Bárbara Souza on 13/04/20.
+//  Copyright © 2020 Bárbara Souza. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func addLineSpace(_ spacing: CGFloat) -> NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = spacing
+        let attributedString = NSAttributedString(string: self,
+                                                  attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        return attributedString
+    }
+}
